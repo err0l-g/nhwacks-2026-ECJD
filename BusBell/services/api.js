3a759713515d556a27445c5d47a3e5369fc41864
Dylan Reimer <reimer.dylan@gmail.com>
2026-01-17 16:08:48 -0800

    filter some stats

--- BusBell/services/api.js
+++ BusBell/services/api.js
@@ -9,7 +9,14 @@ export default async function getAPI(api) {
     const response = await axios.get(api, {responseType: 'arraybuffer'})
     const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(new Uint8Array(response.data));
 
-    console.log(feed.entity[0].id)
+    console.log(feed.entity[0])
+    for (let i = 0; i < feed.entity[0].tripUpdate.stopTimeUpdate.length; i++)
+    {
+      console.log(feed.entity[0].tripUpdate.stopTimeUpdate[i])
+    }
+
+    console.log(feed.entity[0].tripUpdate.trip)
+    console.log(feed.entity[0].tripUpdate.vehicle)
     return feed.entity[0].id
   }
   catch (error)
