e89dc6fd8a780431a1b85840272db4838522d4ca
Erryl Galang <errylgalang@gmail.com>
2026-01-17 16:50:36 -0800

    Add constants for Notification Service

---
+++ BusBell/android/app/src/main/java/com/anonymous/BusBell/alarm/Constants.kt
@@ -0,0 +1,11 @@
+package com.anonymous.BusBell.alarm
+
+object Constants {
+    // Channel Config
+    const val CHANNEL_ID = "BusBell_Channel"
+    const val CHANNEL_NAME = "Bus Alarms"
+
+    // Notification IDs
+    const val NOTIF_ID_FOREGROUND = 1
+    const val NOTIF_ID_ALERT = 2
+}
