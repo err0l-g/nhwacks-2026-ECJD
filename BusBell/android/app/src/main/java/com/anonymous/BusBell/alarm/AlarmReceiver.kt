d004f43646b2d74b75e7ed07303427aaf23a6a92
Erryl Galang <errylgalang@gmail.com>
2026-01-18 09:14:02 -0800

    Add logging

--- BusBell/android/app/src/main/java/com/anonymous/BusBell/alarm/AlarmReceiver.kt
+++ BusBell/android/app/src/main/java/com/anonymous/BusBell/alarm/AlarmReceiver.kt
@@ -3,6 +3,7 @@ package com.anonymous.BusBell.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import androidx.core.content.ContextCompat
 import com.anonymous.BusBell.service.NotificationService
 
@@ -12,17 +13,16 @@ class AlarmReceiver : BroadcastReceiver() {
         val id = intent.getIntExtra(Constants.KEY_ALARM_ID, 0)
         if (id == 0) return
 
-        // 1. Simply wake up the service with the ID
+
         val serviceIntent = Intent(context, NotificationService::class.java).apply {
+            Log.i("AlarmReceiver", "Waking up service")
+
             action = Constants.ACTION_ALARM_TRIGGER
             putExtra(Constants.KEY_ALARM_ID, id)
         }
 
         ContextCompat.startForegroundService(context, serviceIntent)
 
-        // 2. Reschedule for tomorrow (Repeating daily check)
-        // We don't check "Days" here anymore. We just ensure the AlarmManager
-        // keeps firing daily. The DB check in Service decides if we act on it.
         val hour = intent.getIntExtra(Constants.KEY_TARGET_HOUR, 8)
         val min = intent.getIntExtra(Constants.KEY_TARGET_MINUTE, 0)
 
